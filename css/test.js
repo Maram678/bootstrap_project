8f536bcede6c3d426a7f66863231ecbade7ab0a7
Maram678 <maramkhaliddddddd@gmail.com>
2023-10-18 19:22:18 +0300

    first commit

---
+++ css/test.js
@@ -0,0 +1,8 @@
+const nums1 = [1, 1, 2];
+// After removing duplicates, the array becomes [1, 2], and the length is 2
+for(var i;i<length;i++){
+    
+    if(nums[i]==num[i+1])
+    nums[i].shift();
+
+}
